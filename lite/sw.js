e14ddb9ec601dc299bd181bff9fe2526497e5dd5
Liam Anstey <liam@ansteyapps.com>
2026-04-19 07:39:01 +0000

    Add Stroke Sight Lite: free version, 3 levels, all conditions/exercises, upgrade prompt to full

---
+++ lite/sw.js
@@ -0,0 +1,51 @@
+var CACHE_NAME = 'strokesight-lite-v1';
+var ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS);
+    }).then(function() {
+      return self.skipWaiting();
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(n) { return n !== CACHE_NAME; })
+             .map(function(n) { return caches.delete(n); })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    }).then(function() {
+      // Notify all open clients to reload so they get the new version
+      return self.clients.matchAll({ type: 'window' }).then(function(clients) {
+        clients.forEach(function(client) { client.postMessage({ type: 'SW_UPDATED' }); });
+      });
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request).then(function(response) {
+        return caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    }).catch(function() {
+      return caches.match('./index.html');
+    })
+  );
+});
